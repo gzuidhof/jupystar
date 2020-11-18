b2673fbd7f9aa53d60f56a4ce653629d2941cb9f
Guido Zuidhof <me@guido.io>
2020-11-18 03:11:36 +0000

    Add minified build

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,6 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
 import replace from '@rollup/plugin-replace';
+import {terser} from 'rollup-plugin-terser';
 
 const pkg = require("./package.json");
 
@@ -9,6 +10,7 @@ export default {
   input: `src/index.ts`,
   output: [
     {file: 'dist/index.js', format: 'es'},
+    {file: 'dist/index.min.js', format: 'es', plugins: [terser()]},
     {file: 'dist/index.cjs', format: 'cjs'}
   ],
   plugins: [
